8127ec1d94f9f295bf8fd1a3f55f06987371e260
isaDevs <isabel.calzadilla.18@gmail.com>
2022-09-07 18:15:31 +0200

    commit 2 --> start the issues with mapping.

--- src/main/java/com/ironhack/iroronbank_monolit/model/account/Account.java
+++ src/main/java/com/ironhack/iroronbank_monolit/model/account/Account.java
@@ -1,4 +1,48 @@
 package com.ironhack.iroronbank_monolit.model.account;
 
+import com.ironhack.iroronbank_monolit.model.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
+
+    /*
+    * BASICS Attributes of Account
+    * */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; // --- ???
+
+    private BigDecimal balance;
+
+    private String secretKey;
+
+    @OneToOne(mappedBy = "account")
+    private User primaryOwner;
+
+    @OneToOne(mappedBy = "secondaryOwner")
+    private User secondaryOwner;
+
+    private BigDecimal penaltyFee;
+
+    private Date creationDate;
+
+    private Date interestDate;
+
+    private Date transactionDate;  // ---> JUST FOR THE ANTIFRAUD METHOD
+
+
+
+
 }
--- src/main/java/com/ironhack/iroronbank_monolit/model/user/User.java
+++ src/main/java/com/ironhack/iroronbank_monolit/model/user/User.java
@@ -1,4 +1,23 @@
 package com.ironhack.iroronbank_monolit.model.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Inheritance()
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+
 }
